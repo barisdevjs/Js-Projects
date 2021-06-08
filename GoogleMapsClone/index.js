af5f6248a461486c2dc71d01923ebc412f918eab
BARIS <barissavas17@gmail.com>
2021-06-09 00:32:52 +0300

    new project

---
+++ GoogleMapsClone/index.js
@@ -0,0 +1,47 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2Vyd2VyMTciLCJhIjoiY2twbXZoZWNuMDYzaTJubnUyd2NmOXJidCJ9.thPscesMANS5mHnYfA6aAw';
+// it is our key in the mapbox website
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true // third one is optional
+})
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+    // we have to write longitude first. it is sthng with mapbox !!
+}
+
+function errorLocation() {
+setupMap([ 27.532683372024152 , 41.019545547100876]) // TEKİRDAĞ We have to first write longitude then latitude otherwise 
+// we will get falsy values,  
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',  // div id in the html
+        style: 'mapbox://styles/mapbox/streets-v11', // to see the streets we use streets-v11
+        center: center,
+        zoom:10 /* it is the first opening scale of map
+        if you set it to 1 you will see whole earth map */ 
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left')
+
+    const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: 'metric'
+      });  
+      map.addControl(directions, 'top-right');
+
+
+
+document.getElementById('fit').addEventListener('click', function () {
+    map.fitBounds([
+    [32.837716, 39.925039], // southwestern corner of the bounds
+    [32.836120, 39.925039] // northeastern corner of the bounds
+    ]),
+    setTimeout(() => window.open('https://www.anitkabir.tsk.tr/index.html'), 9000) 
+    // This is the page will be opened after the button clicked  
+});
+}
+
