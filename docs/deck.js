10e9a1f67909e4b298929bc4cc82f8a18d7bf1d0
Barış SAVAŞ <barissavas17@gmail.com>
2021-10-27 00:14:57 +0300

    changes
    changed

---
+++ docs/deck.js
@@ -0,0 +1,61 @@
+const SUITS = ['♠','♥','♦','♣']
+const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','Q','K']
+// They are global value so all of the letters are Uppercase letters
+
+export default class Deck {
+    constructor(cards = freshDeck()) {
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    pop() {
+        return this.cards.shift()
+    }
+
+    push(card) {
+        this.cards.push(card)
+    }
+
+    shuffle() {
+        for (let i = this.numberOfCards -1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() *(i+1)) // random index
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue    
+        }
+    }
+}
+
+class Card {
+    constructor(suit,value) {
+        this.suit = suit
+        this.value = value
+    }
+
+    get color() {
+        return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red' 
+    }
+
+    getHTML() {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerHTML = this.suit
+        cardDiv.classList.add('card' , this.color)
+        cardDiv.dataset.value = `${this.value}${this.suit}`
+        return cardDiv
+    }
+}
+/* 
+<div class="card red" data-value='9♥'>
+♥
+</div>
+ */
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit,value)
+        })
+    })
+}
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+# This is a simple card game between AI and Human
+---
+## Below you will see the page of a game
+
+![alt text](https://github.com/barisdevjs/Js-Projects/blob/main/Card-Game/readme%20file%20image.jpg?raw=true)
+
+---
+
+- Whose value's are bigger than its opponent's wins the round
+- Game will be played when one side cards runs out of .
+- Click anywhere to start game on HTML page
+- Good Luck
+---
+
+### This game logic is build by the help of `constructor functions && classes`
+ 
+ 
