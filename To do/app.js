7b2b31a71e6317cfb7a703fc70f9bcbbac227eb1
Barış SAVAŞ <barissavas17@gmail.com>
2023-05-03 14:48:34 +0300

    App Created

---
+++ To do/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const todoList = document.getElementById('todoList');
+  
+    function addTodo() {
+      const value = searchBox.value;
+      if (value.trim() !== '') {
+        const newTodo = document.createElement('div');
+        newTodo.classList.add('todo');
+      
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('checkbox');
+        checkbox.addEventListener('change', () => {
+          if (checkbox.checked) {
+            newTodo.classList.add('done');
+          } else {
+            newTodo.classList.remove('done');
+          }
+        });
+      
+        const label = document.createElement('p');
+        label.textContent = value;
+        label.classList.add('label');
+      
+        const closeButton = document.createElement('button');
+        closeButton.textContent = 'X';
+        closeButton.classList.add('delete-btn');
+        closeButton.addEventListener('click', () => {
+          newTodo.remove();
+        });
+      
+        newTodo.appendChild(checkbox);
+        newTodo.appendChild(label);
+        newTodo.appendChild(closeButton);
+      
+        todoList.appendChild(newTodo);
+        searchBox.value = '';
+      }
+    }
+      
+    const searchBox = document.getElementById('searchBox');
+    const addButton = document.querySelector('button[type=submit]');
+    addButton.addEventListener('click', addTodo);
+    
+    searchBox.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        addTodo();
+      }
+    });
+  });
+  
